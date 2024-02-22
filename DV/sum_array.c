9120cce098c7582cda0b908c00fd43f736cf09e7
agp00118 <agp00118@red.ujaen.es>
2024-02-22 18:28:07 +0100

    Triying to resolve the D and C of sum_array

--- DV/sum_array.c
+++ DV/sum_array.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <limits.h>
 #include <stdlib.h>
+
 // Function to print an array
 void printArray(int A[], int size)
 {
@@ -9,6 +10,66 @@ void printArray(int A[], int size)
         printf("%d ", A[i]);
     printf("\n");
 }
+
+//Function to merge
+void merge (int arr[], int l, int r, int *sum, int *min, int *max)
+{
+    //if (r - l > 2)
+    //{
+        for (int i = l; i < r; i++)
+        {
+            if (*min > arr[i])
+            {
+                *min = arr[i];
+            }
+            if (*max < arr[i])
+            {
+                *max = arr[i];
+            }
+            *sum += arr[i];
+        }
+    //}
+    //else
+    //{
+    //    for (int i = l; i <= r; i++)
+    //    {
+    //        *sum += arr[i];
+    //    }  
+    //}
+}
+
+// Function to divide in two subArrays
+void Divide(int arr[], int l, int r, int *sum, int *min, int *max)
+{
+    int mid = l + (r - l) / 2;
+
+    if (r - l > 2)
+    {
+        Divide(arr, l, mid, sum, min, max);
+        Divide(arr, mid+1, r, sum, min, max);
+
+        merge(arr, l, r, sum, min, max);
+    }
+    else
+    {
+        //TERMINAR ESTO
+        for (int i = 0;)
+    }
+}
+
+// Function Divide and Conquer Way
+int DivConq(int arr[], int l, int r)
+{
+    int min = 0;
+    int max = 0;
+    int sum = 0;
+
+    Divide(arr, l, r, &sum, &min, &max);
+
+    return (sum -= (min + max));
+}
+
+
 
 
 int classicAlgorithm(int arr[], int SIZE){
@@ -39,5 +100,7 @@ int main(){
     printf("The given array is:\n");
     printArray(arr,8);
 
-    printf("The result of the sum except MIN and MAX element is:\n%d\n", classicAlgorithm(arr,8));
+    //printf("The result of the sum except MIN and MAX element is:\n%d\n", classicAlgorithm(arr,8));
+
+    printf("The result of the sum except MIN and MAX element is:\n%d\n", DivConq(arr,0,8-1));
 }
